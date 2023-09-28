a2d50b7563b550cda14570bda604b4f73a80e4cb
Guilherme Casagrande <thegui4000@gmail.com>
2023-09-28 13:35:27 -0300

    feat: ser[a que ta funfando?

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let paragraph = document.querySelector('#localization')
 let i = 0
-navigator.geolocation.watchPosition( (position) => {
-    paragraph.innerHTML += "</br>" + i + ". " + position.coords.latitude + " " + position.coords.longitude;
-    i++
-}, (error) => {console.log(error)})
+
+
+setInterval(()=>{
+    navigator.geolocation.getCurrentPosition((position) => {
+        paragraph.innerHTML += `</br>${i++}. ${position.coords.latitude} ${position.coords.longitude}`
+    })
+}, 1000)
