858dd0b629960db7eec40958b4573fb927a4a853
Guilherme Casagrande <thegui4000@gmail.com>
2023-09-28 10:21:18 -0300

    fix: faltou o i++

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let paragraph = document.querySelector('#localization')
 let i = 0
 navigator.geolocation.watchPosition( (position) => {
-    paragraph.innerHTML += "</br>" + i + position.coords.latitude + " " + position.coords.longitude
+    paragraph.innerHTML += "</br>" + i + ". " + position.coords.latitude + " " + position.coords.longitude;
+    i++
 }, (error) => {console.log(error)})
