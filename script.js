d37b11c3058fbcc976af338ed4f509328dc06df7
Guilherme Casagrande <thegui4000@gmail.com>
2023-09-28 14:02:09 -0300

    feat: mudando a corzinha fru fru

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 let paragraph = document.querySelector('#localization')
-let i = 0
+let i = 1
+let arr = []
 
 
 setInterval(()=>{
     navigator.geolocation.getCurrentPosition((position) => {
-        paragraph.innerHTML += `</br>${i++}. ${position.coords.latitude} ${position.coords.longitude}`
+        if(i % 50 === 0){
+            paragraph.innerHTML = ''
+        }
+        let novo = `${position.coords.latitude} + ${position.coords.longitude}`
+        paragraph.innerHTML += `<span id=span${i}>${i++}. ${position.coords.latitude} ${position.coords.longitude}</span>`
+        if(!arr.includes(novo)){
+            arr.push(novo)
+            let marcelo = document.querySelector(`#span${i - 1}`)
+            console.log(marcelo)
+            marcelo.style.backgroundColor = "#000"
+            marcelo.style.color = "#fff"
+        }
     })
-}, 1000)
+},990)
