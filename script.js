8b8efc0e3142cc57fadafd41b5afdf1c696b4476
Guilherme Casagrande <thegui4000@gmail.com>
2023-09-28 10:02:20 -0300

    feat: att em tempo real

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let paragraph = document.querySelector('#localization')
-navigator.geolocation.getCurrentPosition( (position) => {
-    paragraph.innerHTML = "Sua localização atual é" +  position.coords.latitude + "+" + position.coords.longitude
+navigator.geolocation.watchPosition( (position) => {
+    paragraph.innerHTML = position.coords.latitude + " " + position.coords.longitude
 })
