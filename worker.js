0fa290804e1353975656bbe0e10749d444418857
Guilherme Casagrande <thegui4000@gmail.com>
2023-09-29 14:46:49 -0300

    feat: add, novo marcador pra quando a localização for nova

--- worker.js
+++ worker.js
@@ -2,9 +2,9 @@ self.onmessage = (event) => {
     let tempo = 1000
     var interval
     screen = event.data.screen
-    if (screen.width < 640 || screen.height < 480){
+    /* if (screen.width < 640 || screen.height < 480){
         tempo = 500
-    }
+    } */
     if(event.data.onoff){
         var interval = setInterval(() => {
             self.postMessage(true)
